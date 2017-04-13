58f185e6b114ad049e5fbc9ad0c83c5b2de01a14
Paul Brabet <pbrabet@gmail.com>
2017-04-13 14:42:26 +1200

    0 now displays as default screen digit

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 $(document).ready(function() {
 
-    //Arr array created for symbols to be entered into
+    //Global arr that we push our equation too.
+    let isCleared = true;
+    let str = '0'; //default number on screen is set to 0
     let arr = [];
 
-    //All clear button empties arr and joined
-    $('#AC').click(function() {
+
+    //boolean is used to change what is displayed on the screen as the default will be '0'
+    if (isCleared) {
+        $('#screen').text(str);
+    }
+
+    if (!isCleared) {
         $('#screen').text('');
+    }
+
+
+    //once the AC button is pressed clears the screen by making variables empty.
+    $('#AC').click(function() {
+        $('#screen').text(str);
         joined = '';
         arr = [];
+        isCleared = true;
     });
 
-    //Equals button calculates entries in arr array
+
+    //Once equals is pressed we change the array to a string and run the eval function on it to get desired result.
     $('#equals').click(function() {
-        let joined = arr.join('')
+        let joined = arr.join('');
         joined = eval(joined);
         $('#screen').html(joined);
     });
 
-    //Remaining buttons push corresponding symbol to arr array
+
+    //Pushes calculator button values to  array
     $('.entry').click(function() {
-        var entry = (this.value)
-        $('#screen').text($('#screen').text() + entry)
+        if (isCleared) {
+            isCleared = false;
+            $('#screen').text('');
+        }
+        let entry = (this.value);
+        $('#screen').text($('#screen').text() + entry);
         arr.push(entry);
     });
 
-})
+
+});
